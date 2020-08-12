b2b050800d6a7e5ac32efc7c2495c8e3c7d7b1e1
Thagana <samuelmthwa79@gmail.com>
2020-08-12 23:08:08 +0200

    :rocket: Updated source file

--- application/models.py
+++ application/models.py
@@ -1,11 +1,13 @@
 from application import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
+
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- application/routes.py
+++ application/routes.py
@@ -1,152 +1,67 @@
 from flask import request, jsonify, make_response
-from application import app, db
+from application import app, db, jwt
 from functools import wraps
 from .models import User, Movie
 import uuid
-import jwt
+from flask_jwt_extended import (
+    jwt_required, create_access_token, get_jwt_identity)
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
 
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-
-        if not token:
-            return jsonify({'message' : 'Token is missing!'}), 401
-
-        try: 
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = User.query.filter_by(public_id=data['public_id']).first()
-        except:
-            return jsonify({'message' : 'Token is invalid!'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
-
-@app.route('/user', methods=['GET'])
-@token_required
-def get_all_users(current_user):
-
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
-
-    users = User.query.all()
-
-    output = []
-
-    for user in users:
-        user_data = {}
-        user_data['public_id'] = user.public_id
-        user_data['name'] = user.name
-        user_data['password'] = user.password
-        user_data['admin'] = user.admin
-        output.append(user_data)
-
-    return jsonify({'users' : output})
-
-@app.route('/user/<public_id>', methods=['GET'])
-@token_required
-def get_one_user(current_user, public_id):
-
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
-
-    user = User.query.filter_by(public_id=public_id).first()
-
-    if not user:
-        return jsonify({'message' : 'No user found!'})
-
-    user_data = {}
-    user_data['public_id'] = user.public_id
-    user_data['name'] = user.name
-    user_data['password'] = user.password
-    user_data['admin'] = user.admin
-
-    return jsonify({'user' : user_data})
-
-@app.route('/user', methods=['POST'])
-@token_required
-def create_user(current_user):
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
-
+@app.route('/login', methods=['POST', 'GET'])
+def login():
     data = request.get_json()
 
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+    # Check if there is data sent
+    if not data['username']:
+        return jsonify({'error': 'Username not set'}), 400
+    if not data['password']:
+        return jsonify({'error': 'Password not set'}), 400
 
-    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    user = User.query.filter_by(name=data['username']).first()
+
+    if user is None:
+        return jsonify({'error': 'Could not find user'})
+
+    if check_password_hash(user.password, data['password']):
+        access_token = create_access_token(identity=data['username'])
+        return jsonify(access_token=access_token), 200
+
+    return jsonify({'error': 'Something happend'}), 400
+
+
+@ app.route('/register', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = User.query.filter_by(name=data['username']).first()
+    if user:
+        return jsonify({'error': 'User already exists'}), 200
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(public_id=str(uuid.uuid4()),
+                    name=data['username'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'message' : 'New user created!'})
+    return jsonify({'message': 'New user created!'}), 201
 
-@app.route('/user/<public_id>', methods=['PUT'])
-@token_required
-def make_admin(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
 
-    user = User.query.filter_by(public_id=public_id).first()
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
-    if not user:
-        return jsonify({'message' : 'No user found!'})
 
-    user.admin = True
-    db.session.commit()
-
-    return jsonify({'message' : 'The user has been promoted to Admin!'})
-
-@app.route('/user/<public_id>', methods=['DELETE'])
-@token_required
-def delete_user(current_user, public_id):
-    
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
-
-    user = User.query.filter_by(public_id=public_id).first()
-
-    if not user:
-        return jsonify({'message' : 'No user found!'})
-
-    db.session.delete(user)
-    db.session.commit()
-
-    return jsonify({'message' : 'The user has been deleted!'})
-
-@app.route('/login')
-def login():
-    auth = request.authorization
-
-    if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-
-    user = User.query.filter_by(name=auth.username).first()
-
-    if not user:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-
-        return jsonify({'token' : token.decode('UTF-8')})
-
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-
-@app.route('/api/v1/movies', methods=['GET'])
-@token_required
-def get_all_movies(current_user):
+@ app.route('/api/v1/movies', methods=['GET'])
+@ jwt_required
+def get_all_movies():
 
     movies = Movie.query.all()
 
     if not movies:
-        return jsonify({'massage': 'movie not found!'})    
+        return jsonify({'massage': 'movie not found!'})
     output = []
 
     for movie in movies:
@@ -156,13 +71,14 @@ def get_all_movies(current_user):
         movie_data['description'] = movie.description
         movie_data['rating'] = movie.rating
         movie_data['awards_won'] = movie.awards_won
-   
+
         output.append(movie_data)
 
     return jsonify({'movies': output})
 
+
 @app.route('/api/v1/movies/<movie_id>', methods=['GET'])
-@token_required
+@jwt_required
 def get_one_movie(movie_id):
 
     movie = Movie.query.filter_by(id=movie_id).first()
@@ -177,41 +93,45 @@ def get_one_movie(movie_id):
     movie_data['rating'] = movie.rating
     movie_data['awards_won'] = movie.awards_won
 
-
     return jsonify(movie_data)
 
+
 @app.route('/api/v1/movies', methods=['POST'])
-@token_required
+@jwt_required
 def add_movie():
 
     data = request.get_json()
 
-    new_movie = Movie(name=data['name'], description=data['description'],rating=data['rating'], awards_won=data['awards_won'])
+    new_movie = Movie(name=data['name'], description=data['description'],
+                      rating=data['rating'], awards_won=data['awards_won'])
     db.session.add(new_movie)
     db.session.commit()
 
-    return jsonify({'massage':'A Movie was added!'})
+    return jsonify({'massage': 'A Movie was added!'})
+
 
 @app.route('/api/v1/movies/<movie_id>', methods=['DELETE'])
 def delete_movie(movie_id):
     movie = Movie.query.filter_by(id=movie_id).first()
 
     if not movie:
-        return jsonify({'massage':'Not Matching Movie found!'})
+        return jsonify({'massage': 'Not Matching Movie found!'})
 
     db.session.delete(movie)
     db.session.commit()
 
-    return jsonify({'massage':'movie deleted succesfully'})
+    return jsonify({'massage': 'movie deleted succesfully'})
+
 
 @app.route('/api/v1/movies/<movie_id>', methods=['PUT'])
 def update_movie(movie_id):
     movie = Movie.query.filter_by(id=movie_id)
 
     if not movie:
-        return jsonify({'massage':'No movie found'})
+        return jsonify({'massage': 'No movie found'})
     data = request.get_json()
 
-    update_mov = Movie(name=data['name'], description=data['description'],rating=data['rating'], awards_won=data['awards_won'])
+    update_mov = Movie(name=data['name'], description=data['description'],
+                       rating=data['rating'], awards_won=data['awards_won'])
     db.session.add(update_mov)
     db.session.commit()
