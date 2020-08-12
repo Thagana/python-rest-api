b3847fbc4e3b0b990d547f0fd62d047820056e00
Thagana <samuelmthwa79@gmail.com>
2020-08-12 23:07:43 +0200

    :rocket: updated file

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import datetime
-
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-
+jwt = JWTManager(app)
 db = SQLAlchemy(app)
 
 
